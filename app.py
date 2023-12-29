b462fe0f96f6f26009cb2916a19958a87430d083
LEOQuester <induwaralakindu09@gmail.com>
2023-12-29 11:24:12 +0530

    completed apis for fetching movies and tv s

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory, request, jsonify
 from firebaseConfig.firebaseConfig import db
-from service import create_user, login_user
+from service import *
 
 app = Flask(__name__)
 
@@ -20,7 +20,32 @@ def login():
     login_data = request.get_json()
     return jsonify(login_user(login_data))
 
+@app.route('/movies')
+def get_movies():
+    keywords = ['fighting', 'adventures', 'romantic']
+    return jsonify(media_from_keywords(keywords, "movie"))
       
+@app.route('/api/media', methods=['GET'])
+def get_media():
+    # Get parameters from the query string
+    title = request.args.get('title', default='', type=str)
+    media_type = request.args.get('media_type', default='', type=str)
+
+    if not title or not media_type:
+        return jsonify({"error": "Both 'title' and 'media_type' parameters are required."}), 400
+
+    if media_type == "movie" or media_type == "tv":
+        result = media_from_title(title=title, media_type=media_type)
+    elif media_type == "song":
+        return "spotify API under development"   
+    elif media_type == "book":
+        return "book fetching api under development"
+
+    if result is not None:
+        return jsonify(result)
+    else:
+        return jsonify({"error": "An error occurred while fetching media data."}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
