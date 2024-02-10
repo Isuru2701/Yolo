9b0feae3e2e71c08165887dabf6f52cfa357625e
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-02-10 08:23:38 +0530

    added payment gateway, keys in .env

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify, redirect
 from firebaseConfig.firebaseConfig import db
 from service import *
 from flask_cors import CORS
+import stripe
+import os
+from dotenv import load_dotenv
+import json
 
 from firebaseConfig.FirebaseDriver import FirebaseDriver
 
@@ -9,6 +13,11 @@ app = Flask(__name__)
 
 CORS(app)
 
+load_dotenv() # loads env variables from project's root
+
+stripe.api_key = os.getenv("STRIPE_API_KEY")
+
+YOUR_DOMAIN = 'http://localhost:4242' # Change later
 
 @app.route('/', methods=['GET'])  # serves UI
 def serve_index():
@@ -184,48 +193,144 @@ def get_media():
 
 
 # developer API endpoint
-@app.route('/developers')
-def fetchInfo(name: str):
-    # TODO: if it's been a month, reset the quota
-    # Note: All tokens share the same quota
-    pass
 
 
-@app.route('/developers/generate')
+@app.route('/developers/generate', methods=['POST'])
 def generateToken():
     pass
 
 
-@app.route('/developers/invalidate')
+@app.route('/developers/invalidate', methods=['POST'])
 def invalidateToken():
     pass
 
 
 # developer related API endpoints
-@app.route('/developers')
+@app.route('/developers', methods=['POST'])
 def fetch_dev_info(name: str):
     # TODO: if it's been a month, reset the quota
     # Note: All tokens share the same quota
     pass
 
 
-@app.route('/developers/generate')
+@app.route('/developers/generate', methods=['GET'])
 def generateApiToken():
     pass
 
 
-@app.route('/developers/invalidate')
+@app.route('/developers/invalidate', methods=['GET'])
 def invalidateApiToken():
     pass
 
 
-@app.route('/api/', method=['POST'])
+@app.route('/api/', methods=['POST'])
 def processRequest():
     """
     every api call must have the following:
 
     :return:
     """
+
+
+# PAYMENT GATEWAY
+@app.route('/payment/', methods=['POST'])
+def make_payment():
+    details = request.get_json()
+
+
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    """
+    request must have:
+    - lookup_key
+
+    :return:
+    """
+
+    try:
+        prices = stripe.Price.list(
+            lookup_keys=[request.form['lookup_key']],
+            expand=['data.product']
+        )
+
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': prices.data[0].id,
+                    'quantity': 1,
+                },
+            ],
+            mode='subscription',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html'
+            ,
+        )
+        return redirect(checkout_session.url, code=303)
+    except Exception as e:
+        print(e)
+        return "Server error", 500
+
+@app.route('/create-portal-session', methods=['POST'])
+def customer_portal():
+    # For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
+    # Typically this is stored alongside the authenticated user in your database.
+    checkout_session_id = request.form.get('session_id')
+    checkout_session = stripe.checkout.Session.retrieve(checkout_session_id)
+
+    # This is the URL to which the customer will be redirected after they are
+    # done managing their billing with the portal.
+    return_url = YOUR_DOMAIN
+
+    portalSession = stripe.billing_portal.Session.create(
+        customer=checkout_session.customer,
+        return_url=return_url,
+    )
+    return redirect(portalSession.url, code=303)
+
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook_received():
+    # Replace this endpoint secret with your endpoint's unique secret
+    # If you are testing with the CLI, find the secret by running 'stripe listen'
+    # If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+    # at https://dashboard.stripe.com/webhooks
+    webhook_secret = os.getenv("WEBHOOK_SECRET")
+    request_data = json.loads(request.data)
+
+    if webhook_secret:
+        # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+        signature = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=request.data, sig_header=signature, secret=webhook_secret)
+            data = event['data']
+        except Exception as e:
+            return e
+        # Get the type of webhook event sent - used to check the status of PaymentIntents.
+        event_type = event['type']
+    else:
+        data = request_data['data']
+        event_type = request_data['type']
+    data_object = data['object']
+
+    print('event ' + event_type)
+
+    if event_type == 'checkout.session.completed':
+        print('🔔 Payment succeeded!')
+    elif event_type == 'customer.subscription.trial_will_end':
+        print('Subscription trial will end')
+    elif event_type == 'customer.subscription.created':
+        print('Subscription created %s', event.id)
+    elif event_type == 'customer.subscription.updated':
+        print('Subscription created %s', event.id)
+    elif event_type == 'customer.subscription.deleted':
+        # handle subscription canceled automatically based
+        # upon your subscription settings. Or if the user cancels it.
+        print('Subscription canceled: %s', event.id)
+
+    return jsonify({'status': 'success'})
 
 
 
